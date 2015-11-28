447ca5dfa3ce9b12ddb3ed7a8ee6bbb00c92e5ff
Wilhelmshaven <nntt19@163.com>
2015-11-28 21:03:49 +0800

    add new problems

---
+++ LeetCode OJ/Missing Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+	int missingNumber(vector<int>& nums) {
+		int size = nums.size();
+		int sum = size*(size + 1) / 2;
+		for (int i = 0; i<size; ++i)sum -= nums[i];
+		return sum;
+	}
+};
+//Runtime: 36 ms
+//Your runtime beats 23.51% of cpp submissions.
